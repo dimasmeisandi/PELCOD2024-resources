c384d9d8bad55397c3e004521d3750eead3a5519
dimasmeisandi <dimasmeisandi@gmail.com>
2024-09-21 15:15:26 +0700

    latihan coding 21 september 2024 bismillah calon programer handal

---
+++ main.py
@@ -0,0 +1,21 @@
+""""
+nilai = int(input ("masukkan nilai: "))
+if (nilai > 80):
+    print("selamat anda lulus")
+elif (nilai > 90 and nilai <= 100):
+    print ("selamat anda lulus, dengan nilai yang memuaskan")
+
+else:
+    print("anda masih belum lulus tes, semangat dan coba lagi")
+"""
+"""
+# for loop
+for i in range(1, 10, -2):
+    print (i)
+    print("Hello world")
+"""
+
+angka = 30
+
+while (angka > 1):
+    print(angka)
